d9847653d8ab9089d914f9a5629598ba55b858e8
Taylor Huffman <taylor@crucialartists.com>
2022-01-11 22:57:35 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(parameter) {
+    return parameter.slice(0,2);
+}
+
+function returnLastTwoDrivers(parameter) {
+    return parameter.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function() {
+        return fare * fare;
+    }
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, fn) {
+    return fn(arrayOfDrivers);
+}
